561961c72bf6d9a356187d7c60f40840e6469786
tahimgit <alamtahim3@gmail.coom>
2024-05-14 23:19:29 +0600

    remove unnessary code

--- index.js
+++ index.js
@@ -33,10 +33,6 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
 
-
-
-
-        
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -46,9 +42,6 @@ async function run() {
     }
 }
 run().catch(console.dir);
-
-
-
 
 
 app.get('/', (req, res) => {
